43b1524dcda6cccdeefe0b34c813e0dff2cd2c1b
mhmd ali <serever100@gmail.com>
2024-09-14 23:57:34 +0330

    removed base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-online-shop/",
+  // base: "/react-online-shop/",
   plugins: [react()],
 });
